6a72f75ecb4531eaca18ee92e30d19aa062b7a62
Johnatas Souza <109979241+John1Souza@users.noreply.github.com>
2024-07-10 17:46:05 -0300

    [ATUALIZACAO] ...

--- src/main/java/com/spring/concepts/controllers/ProdutoController.java
+++ src/main/java/com/spring/concepts/controllers/ProdutoController.java
@@ -30,4 +30,14 @@ public class ProdutoController {
         produtoRepository.save(produto);
         return produto;
     }
+
+    @GetMapping
+    public Iterable<Produto> obterProdutos(){
+        return produtoRepository.findAll();
+    }
+
+    @GetMapping(path="/{id}")
+    public Optional<Produto> obterProdutoPorId(@PathVariable int id){
+        return produtoRepository.findById(id);
+    }
 }
