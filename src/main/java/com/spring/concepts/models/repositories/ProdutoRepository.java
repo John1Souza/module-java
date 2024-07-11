58c55ce8ff0dd60a45d256c932dbced0e49f45a5
Johnatas Souza <109979241+John1Souza@users.noreply.github.com>
2024-07-11 16:11:35 -0300

    [ATUALIZACAO] Pesquisa por nome

--- src/main/java/com/spring/concepts/models/repositories/ProdutoRepository.java
+++ src/main/java/com/spring/concepts/models/repositories/ProdutoRepository.java
@@ -3,5 +3,7 @@ package com.spring.concepts.models.repositories;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
-    
+    public Iterable<Produto> findByNomeContainingIgnoreCase(String parteNome){
+
+    }
 }
